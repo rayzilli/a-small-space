26550373afe1e79bfd549c9748ca0fecd07f5d45
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-06-16 21:59:34 -0400

    change animation bounce

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,19 +5,24 @@ module.exports = {
 		extend: {
 			animation: {
 				// Bounces 5 times 1s equals 5 seconds
-				'bounce-short': 'bounce 5s ease-in-out 1'
+				'bounce-short': 'bounce 7s  linear forwards' 
+				  
 			  },
 			  keyframes: {
 
 				 bounce: {
 					'0%, 100%' : {
-					  'transform': 'translateY(0)',
+					  'transform': 'translateY(-80%)',
 					  'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
 					},
 					'50%' :{
 					  'transform': 'translateY(200%)',
 					  'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
-					}
+					}, 
+					'100%' : {
+						'transform': 'translateY(50%)',
+						'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+					  },
 				  }
 			  }
 		},
